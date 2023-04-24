acad122fa246d2426c8693ffb2f2d5405625a6dd
Roni Akiola <roni.akiola@hotmail.com>
2023-04-24 13:15:13 +0300

    trying out form submit

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const taskList = [];
+
+const newTaskBtn = document.querySelector('.button--new');
+const todoForm = document.querySelector('.todo__form');
+
+newTaskBtn.addEventListener('click', () => {
+  // todoForm.classList.add('show');
+  todoForm.classList.remove('hidden');
+});
+
+todoForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = document.querySelector('input-title').value;
+  const deadline = document.querySelector('input-deadline').value;
+  const status = document.querySelector('input-status').value;
+
+  console.log(title, deadline, status);
+
+  const newTask = {
+    title,
+    deadline,
+    status,
+  };
+});
