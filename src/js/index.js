af5f0055a06b3b733f8197b616db7e516478ec78
Roni Akiola <roni.akiola@hotmail.com>
2023-04-24 14:25:20 +0300

    printTask function done

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,12 @@
 
 const taskList = [];
 
-const newTaskBtn = document.querySelector('.button--new');
 const todoForm = document.querySelector('.todo__form');
+const todoList = document.querySelector('.todo__tasks');
+const title = document.querySelector('.input-title');
+const deadline = document.querySelector('.input-deadline');
+const status = document.querySelector('.input-status');
+const newTaskBtn = document.querySelector('.button--new');
 
 newTaskBtn.addEventListener('click', () => {
   // todoForm.classList.add('show');
@@ -12,15 +16,59 @@ newTaskBtn.addEventListener('click', () => {
 
 todoForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  const title = document.querySelector('input-title').value;
-  const deadline = document.querySelector('input-deadline').value;
-  const status = document.querySelector('input-status').value;
+  const titleVal = title.value;
+  const deadlineVal = deadline.value;
+  const statusVal = status.value;
 
-  console.log(title, deadline, status);
+  console.log(titleVal, deadlineVal, statusVal);
 
   const newTask = {
-    title,
-    deadline,
-    status,
+    id: Date.now(),
+    titleVal,
+    deadlineVal,
+    statusVal,
   };
+
+  taskList.push(newTask);
+  printTask(newTask);
+  todoForm.reset();
 });
+
+const printTask = (newTask) => {
+  const task = document.createElement('div');
+  task.classList.add('task');
+
+  const taskContainer = document.createElement('div');
+  taskContainer.classList.add('task__container');
+
+  const taskTitle = document.createElement('h1');
+  taskTitle.innerText = newTask.titleVal;
+  taskTitle.classList.add('task__title');
+
+  const taskDeadline = document.createElement('p');
+  taskDeadline.innerText = newTask.deadlineVal;
+  taskDeadline.classList.add('task__deadline');
+
+  const taskStatus = document.createElement('p');
+  taskStatus.innerText = newTask.statusVal;
+  taskStatus.classList.add('task__status');
+
+  const editButton = document.createElement('button');
+  editButton.innerHTML = 'Edit';
+  editButton.classList.add('button--edit');
+
+  const deleteButton = document.createElement('button');
+  deleteButton.innerHTML = 'Delete';
+  deleteButton.classList.add('button--delete');
+
+  console.log(newTask);
+  console.log(newTask.titleVal);
+
+  task.appendChild(taskContainer);
+  taskContainer.append(taskTitle, taskDeadline, taskStatus);
+  task.appendChild(taskContainer);
+  task.appendChild(editButton);
+  task.appendChild(deleteButton);
+
+  todoList.appendChild(task);
+};
